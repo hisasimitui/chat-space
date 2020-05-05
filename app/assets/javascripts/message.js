8117041ce5e19d1b91970b550583a504effe2b90
hisasimitui <hisasimitui@icloud.com>
2020-05-05 14:50:49 +0900

    非同期通信修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id="${message.id}">
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id="${message.id}">
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -61,6 +61,32 @@ $('#new_message').on('submit', function(e){
   .fail(function(){
     alert('error');
   })
-
+  
 })
+var reloadMessages = function() {  
+  const last_message_id = $(".message:last").data('message-id');
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    console.log(messages)
+    if (messages.length !== 0) {
+    var insertHTML = '';
+    $.each(messages, function(i, message) {
+      insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);  
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+    }
 });
